52f47ef0a2e75e8a6a177f5dd8895bae624bdb3b
Camron Criddell <criddellcamron1@gmail.com>
2025-10-01 09:17:51 -0400

    Start of project

---
+++ TaskManager/src/body.jsx
@@ -0,0 +1,16 @@
+function body(){
+
+    return(
+        <p><h1>
+            Welcome to task Manager
+            </h1>
+            <h3>Task manager</h3>
+            <h5>Please enter in your details to be logged in</h5>
+            </p>
+            
+            // display the header\\ 
+            
+
+    );
+}
+export default body
