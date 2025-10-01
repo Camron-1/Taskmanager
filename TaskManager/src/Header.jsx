da6c91b318c40e7629759bb259d8a737ecf1d5ec
Camron Criddell <criddellcamron1@gmail.com>
2025-10-01 10:07:02 -0400

    updates to header files

--- TaskManager/src/Header.jsx
+++ TaskManager/src/Header.jsx
@@ -1,13 +1,26 @@
+import './Header.css'
+
 function Header(){
 
     return(
-        <header>
+        <header className="app-header">
             <h1>Welcome to Task Manager</h1>
             <h3>Task Manager</h3>
-            <p>This is a powerful task management application that helps you organize your daily activities, track your progress, and stay productive. Create, edit, and manage your tasks with ease.</p>
+            <p>This is a simple task management application that helps you organize your daily activities, track your progress, and stay productive. Create, edit, and manage your tasks with ease.</p>
             
             <h5>Please enter in your details to be logged in</h5>
-           
+           <h3> Email </h3>
+           <email> <input type="email" placeholder="Enter your email" /> </email>
+           <br></br>
+           <h3> Password </h3>
+           <password> <input type="password" placeholder="Enter your password" /> </password>
+           <br></br>
+           <br></br>
+           <button>Login</button>
+           <br></br>
+           <br></br>
+           <h3> Forgot your password? </h3>
+           <button>Reset password</button>
             
             
         </header>
